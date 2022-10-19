b6eccfd477b09667990ed75df71688a16a238eb9
D0ngHyeon <zmdkdna2@naver.com>
2022-10-19 17:28:45 +0900

    캐릭터 에셋 추가
    1. 캐릭터 에셋 추가
    2. Test 코드 작성

---
+++ Assets/1. Script/DungeonGeneration/DungeonGenerationData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DungeonGenerationData.asset", menuName = "DungeonGenerationData/Dungeon Data")]
+
+public class DungeonGenerationData : MonoBehaviour
+{
+    public int numberOfCrawlers;
+    public int iterationMin;
+    public int iterationMax;
+
+}
